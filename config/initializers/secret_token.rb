83ff1c2b5601a13083bde70c13eb6dd669d725f1
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook-Pro.local>
2011-05-03 22:43:47 -0300

    Rails template with: activerecord, jammit, jquery, omniauth, envyaml, slim, cucumber, rspec, sass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ankara::Application.config.secret_token = '0a24ba547dd8b80fa339805cd0bf30c78440ec1d7963258b0f460c19b3da1aedb06a8d7f8f1ceb0f4162fe3b6e798171767f6af791600395a1379be10f83a13f'
